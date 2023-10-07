e0675ef4562b80cfc043aba0145a0c0c97c501d9
Aaditya Diwan <aadityapdiwan@gmail.com>
2023-10-07 08:33:17 +0530

    Initial commit

---
+++ test/appsync-dynamodb-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AppsyncDynamodbDemo from '../lib/appsync-dynamodb-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/appsync-dynamodb-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AppsyncDynamodbDemo.AppsyncDynamodbDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
